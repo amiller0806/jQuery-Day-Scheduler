d076d35ad4751cfab28747ea80aa0fca9a03a34f
amiller0806 <ariel.miller555@gmail.com>
2022-10-18 10:38:32 -0400

    for loop in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
-$(".savePlan").on("click", function(e) {
-    e.preventDefault();
-    var planToSave = $(this).parent().siblings(".col-7").children(".userentry").val();
-    var timeBlock = $(this).parent().siblings(".col-7").children(".userentry").attr("id");
+$(".savePlan").on("click", function (e) {
+  e.preventDefault();
+  var planToSave = $(this).parent().siblings(".col-7").children(".userentry").val();
+  var timeBlock = $(this).parent().siblings(".col-7").children(".userentry").attr("id");
   console.log(planToSave, timeBlock);
-
+  localStorage.setItem(timeBlock, planToSave);
 })
+for (var i = 9; i < 22; i++) {
+  var saveLocalStorage = localStorage.getItem(i);
+  $("#" + i).val(saveLocalStorage);
+}
+
+
+
+// https://teamtreehouse.com/community/how-do-i-use-a-loop-to-change-the-color-of-elements-within-a-variable 
+// INSPO:
